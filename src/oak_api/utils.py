cd27e1646c6a97445f7f096496788819bf1c0dbf
Patrick Kalita <pkalita@lbl.gov>
2022-07-21 15:52:01 -0700

    Replace deprecated method names

--- src/oak_api/utils.py
+++ src/oak_api/utils.py
@@ -5,12 +5,12 @@ from src.oak_api.ontology import implementation
 
 
 def get_classes_from_curies(curies: Iterable[str]) -> Iterable[OntologyClass]:
-    labeled = implementation.get_labels_for_curies(curies)
+    labeled = implementation.labels(curies)
     return [
         OntologyClass(
             id=curie,
             label=label,
-            definition=implementation.get_definition_by_curie(curie),
+            definition=implementation.definition(curie),
         )
         for curie, label in labeled
     ]
